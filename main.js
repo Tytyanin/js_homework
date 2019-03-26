ea07c7a0f35b2bfbe00ce32d0d4ee2889bf5f6b7
Yaroslav Tytyanin <yarik_tit@ukr.net>
2019-03-26 19:12:14 +0200

    урок 15

---
+++ main.js
@@ -0,0 +1,75 @@
+var myString = 'Studying javascript';
+    myNumber = 25;
+    myBoolean = true;
+    myNull = null;
+    myUndef = undefined;
+
+    console.log(myString);
+    console.log(myNumber);
+    console.log(myBoolean);
+    console.log(myNull);
+    console.log(myUndef);
+
+var myNumber = 250;
+    myString = 'Studying javascript';
+    myBoolean = true;
+    myNull = null;
+    myUndef = undefined;
+
+    console.log(typeof myNumber);
+    console.log(typeof myString);
+    console.log(typeof myBoolean);
+    console.log(typeof myNull); 
+    console.log(typeof myUndef);
+
+
+var myNumb = 60.5;
+    console.log(myNumb.toFixed(2));
+
+var i = 100;
+    console.log(i++);
+    console.log(i);
+    console.log(++i);
+
+var i = 100;
+    console.log(i--);
+    console.log(i);
+    console.log(--i);
+
+var a = 10;
+    a +=5;
+    console.log(a);
+var b = 10;
+    b -=5;
+    console.log(b);
+var c = 20;
+    c *=5;
+    console.log(c);
+var d = 500;
+    d /=5;
+    console.log(d);
+var e = 13;
+    e %=5;
+    console.log(e);
+
+console.log(Math.PI);
+
+var n = 89.279;
+    console.log(Math.round(n));
+
+console.log(Math.round(Math.random()*10));
+
+console.log(Math.pow(2,5));
+
+var myString = 'Lorem ipsum dolor sit amet';
+    console.log(myString);
+    console.log(myString.length);
+    console.log(myString.substring(10,15));
+    console.log(myString.indexOf('dolo'));
+    console.log(myString.replace('dolor','newcode'));
+
+var myStr = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum, vero.';
+    console.log(myStr.toUpperCase());
+    console.log(myStr.toLowerCase());
+    console.log(myStr.split(' '));
+    console.log(myStr.split(''));
